8078cfa69479fa340be631f41cfcedeed58e0627
Mike Petrov <mis5116@yandex.ru>
2019-03-02 21:33:42 +0300

    Initial commit

---
+++ precache-manifest.918b74de280cd84df4f1b07472178a7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "a89d66046955bd64d35d",
+    "url": "/static/js/main.a89d6604.chunk.js"
+  },
+  {
+    "revision": "c88c70e5f4ff8bea6fac",
+    "url": "/static/js/2.c88c70e5.chunk.js"
+  },
+  {
+    "revision": "a89d66046955bd64d35d",
+    "url": "/static/css/main.be9cd952.chunk.css"
+  },
+  {
+    "revision": "4b2a5dc010f9f13e62583fb5b235e71c",
+    "url": "/index.html"
+  }
+];
