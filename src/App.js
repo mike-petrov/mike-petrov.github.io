8fc7ce6369f90a38e5c0dc7e3fc9cea9f5f8cf21
Mike Petrov <mis5116@yandex.ru>
2019-03-03 21:12:57 +0300

    update

--- src/App.js
+++ src/App.js
@@ -3,8 +3,26 @@ import React from 'react';
 class App extends React.Component {
     render() {
         return (
-            <div>
-                1
+            <div className='about table'>
+                <div className='table-cell'>
+                    <div className='content'>
+                        <div className='title'>
+                            Mike Petrov
+                        </div>
+                        <img src="https://avatars0.githubusercontent.com/u/32885629"/>
+                        <div className='description'>
+                            TEXT
+                        </div>
+                        <div className='social'>
+                            <ul>
+                                <li><a class="social-link" href="https://vk.com/mike_petrov">VK</a></li>
+                                <li><a class="social-link" href="https://t.me/mike_petrov">Telegramm</a></li>
+                                <li><a class="social-link" href="mailto:mis5116@yandex.ru">Email</a></li>
+                                <li><a class="social-link" href="https://github.com/mike-petrov">GitHub</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
