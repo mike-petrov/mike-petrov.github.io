1234601fbeef932598a4b545ba6038a4530a6f6e
mike-petrov <mis5116@yandex.ru>
2019-06-27 17:28:24 +0300

    split on components

--- src/App.js
+++ src/App.js
@@ -1,36 +1,27 @@
 import React from 'react';
+import {  BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import Main from './Components/Main';
+import CV from './Components/CV';
+import Portfolio from './Components/Portfolio';
+
 
 class App extends React.Component {
     render() {
         return (
-            <div className='about table'>
-                <div className='table-cell'>
-                    <div className='content color0'>
-                        <div className='title'>
-                            Mike Petrov
-                        </div>
-                        <img src="https://avatars0.githubusercontent.com/u/32885629"/>
-                        <div className='status'>
-                            Reactman
-                        </div>
-                        <div className='social'>
-                            <ul>
-                                <li><a className="social-link" href="https://vk.com/mike_petrov">VK</a></li>
-                                <li><a className="social-link" href="https://t.me/mike_petrov">Telegramm</a></li>
-                                <li><a className="social-link" href="mailto:mis5116@yandex.ru">Email</a></li>
-                                <li><a className="social-link" href="https://github.com/mike-petrov">GitHub</a></li>
-                            </ul>
-                        </div>
-                        <div className='hackathons'>
-                            <ul>
-                                <li><a className="social-link" href="/cv">CV</a></li>
-                                <li><a className="social-link" href="https://github.com/mike-petrov/Hackathons">Hackathons</a></li>
-                                <li><a className="social-link" href="/portfolio">Portfolio</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
+			<BrowserRouter>
+				<Switch>
+                    <Route exact path="/">
+                        <Main />
+                    </Route>
+                    <Route path="/cv">
+                        <CV />
+                    </Route>
+                    <Route path="/portfolio">
+                        <Portfolio />
+                    </Route>
+			   </Switch>
+	   		</BrowserRouter>
         );
     }
 }
