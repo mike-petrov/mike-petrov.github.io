2d7c5f7f4e830c47848b012b3cfb6c43e4e946e9
mike-petrov <mis5116@yandex.ru>
2020-03-13 20:14:32 +0300

    redirect release

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-
-import Home from './Components/Home';
 
 
 class App extends React.Component {
+    componentWillMount() {
+        window.location.replace("https://mikepetrov.ru");
+    }
+
     render() {
         return (
-			<BrowserRouter>
-				<Switch>
-                    <Route exact path="/">
-                        <Home />
-                    </Route>
-			   </Switch>
-	   		</BrowserRouter>
+			<div></div>
         );
     }
 }
